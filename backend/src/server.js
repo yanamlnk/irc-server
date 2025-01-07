357a4dddfd4b73a87cdc4e43e596c77dce6cf180
Luiza Nobrega <mmluiza_@hotmail.com>
2025-01-07 17:09:38 +0100

    server socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,20 @@
 const http = require('http');
 const app = require('./app');
 
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+});
+
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -34,8 +48,6 @@ const errorHandler = error => {
       throw error;
   }
 };
-
-const server = http.createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
